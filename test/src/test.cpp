15d4753c31eb7cbf2b899394ab5ff6dcf50bbc37
roee168 <roee168@gmail.com>
2014-08-31 19:57:24 +0300

    start

---
+++ test/src/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 23 באוג 2014
+ *      Author: Admin
+ */
+/*
+ * this program will print "hello word"
+ */
+#include <iostream>
+#include "stdio.h"
+int main(){
+	std::cout<<"hello word"<<std::endl;//prints hello word
+	return 0;
+}
+
+
+
